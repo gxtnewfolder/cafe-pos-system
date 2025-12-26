bcc05b062c235f24627bfe732d00bdd17f5b252d
gxtnewfolder <annsw.work@gmail.com>
2025-12-27 04:57:00 +0700

    feat: integrate Turso Cloud support and update Prisma configuration

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,40 @@
-import { PrismaClient } from '@/app/generated/prisma/client';
-import { PrismaLibSql   } from '@prisma/adapter-libsql';
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
-const adapter = new PrismaLibSql({
-    url: process.env.DATABASE_URL || "file:./dev.db",
+const adapterlibsql = new PrismaLibSql({
+  url: process.env.DATABASE_URL || "file:./dev.db",
 });
 
 const prismaClientSingleton = () => {
+  const tursoUrl = process.env.TURSO_DATABASE_URL;
+  const tursoAuthToken = process.env.TURSO_AUTH_TOKEN;
+
+  // 1. กรณีขึ้น Cloud (Turso)
+  if (tursoUrl && tursoAuthToken) {
+    console.log("🔌 Connecting to Turso Cloud...");
+    const libsql = createClient({
+      url: tursoUrl,
+      authToken: tursoAuthToken,
+    });
+
+    const adapter = new PrismaLibSql(libsql as any);
+
     return new PrismaClient({ adapter });
-}
+  }
+
+  // 2. กรณี Local (SQLite)
+  console.log("💻 Connecting to Local SQLite...");
+
+  return new PrismaClient({ adapter: adapterlibsql });
+};
+
 declare const globalThis: {
-    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
+
 const prisma = globalThis.prismaGlobal || prismaClientSingleton();
 
 export default prisma;
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
--- prisma.config.ts
+++ prisma.config.ts
@@ -11,5 +11,6 @@ export default defineConfig({
   },
   datasource: {
     url: process.env["DATABASE_URL"],
+    shadowDatabaseUrl: process.env["TURSO_DATABASE_URL"],
   },
 });
